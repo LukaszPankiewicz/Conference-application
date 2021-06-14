0c058a5b294b6c48bae73eaddb913931acfd2e04
Lukasz Pankiewicz <pankiewiczlukash@gmail.com>
2021-06-14 14:12:17 +0200

    First commit

---
+++ src/main/java/pl/pankiewicz/springbootconference/SpringbootConferenceApplication.java
@@ -0,0 +1,13 @@
+package pl.pankiewicz.springbootconference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConferenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pankiewicz/springbootconference/SpringbootConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pankiewicz.springbootconference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConferenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
